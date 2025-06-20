7fd0d99a47c207677ae8355096d05dc29910be39
Dauda <dauda.a.tijani@gmail.com>
2025-06-20 18:42:02 +0100

    feat: enhance getAllTags function to update and return accurate usage counts

--- controllers/tagsController.js
+++ controllers/tagsController.js
@@ -14,7 +14,35 @@ const gatAllTags = async (req, res) => {
       { $unwind: '$tags' },
       { $group: { _id: '$tags', count: { $sum: 1 } } },
     ]);
-  } catch (err) {}
+
+    // Update usage counts and add to response
+    const tagsWithCounts = await Promise.all(
+      (
+        await tags
+      ).map(async (tag) => {
+        const countData = tagCounts.find(
+          (c) => c._id.toString() === tag._id.toString()
+        );
+        const actualCount = countData ? countData.count : 0;
+
+        // Update usage count in database if different
+        if (tag.usageCount !== actualCount) {
+          await Tag.findByIdAndUpdate(tag._id, { usageCount: actualCount });
+        }
+
+        return {
+          ...tag.toObject(),
+          usageCount: actualCount,
+        };
+      })
+    );
+
+    res.json({ tags: tagsWithCounts });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
+
+const getSingleTag = async (req, res) => {};
 
 module.exports = { getAllTags };
