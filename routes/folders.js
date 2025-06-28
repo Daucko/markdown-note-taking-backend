3bc5472be2e4778898a81bdab79a11cfcfc7c12c
Dauda <dauda.a.tijani@gmail.com>
2025-06-28 16:20:16 +0100

    fix: import folderController in folders.js

--- routes/folders.js
+++ routes/folders.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const checkIsVerified = require('../middleware/checkIsVerified');
 const verifyJWT = require('../middleware/verifyJWT');
+const folderController = require('../controllers/folderController');
 
 const router = express.Router();
 
