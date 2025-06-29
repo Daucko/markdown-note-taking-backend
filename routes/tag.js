0ea59f69483b5e8a7b2de2eab456129bf75f159d
Dauda <dauda.a.tijani@gmail.com>
2025-06-29 14:26:18 +0100

    style: format code for consistency in tag routes

--- routes/tag.js
+++ routes/tag.js
@@ -6,18 +6,22 @@ const tagController = require('../controllers/tagController');
 const router = express.Router();
 
 router
-    .route('/')
-    .get(verifyJWT, tagController.getAllTags)
-    .post(verifyJWT, tagController.createNewTag);
+  .route('/')
+  .get(verifyJWT, tagController.getAllTags)
+  .post(verifyJWT, tagController.createNewTag);
 
 router
-    .route('/:id')
-    .get(verifyJWT, tagController.getSingleTag)
-    .patch(verifyJWT, tagController.updateTag)
-    .delete(verifyJWT, tagController.deleteTag)
+  .route('/:id')
+  .get(verifyJWT, tagController.getSingleTag)
+  .patch(verifyJWT, tagController.updateTag)
+  .delete(verifyJWT, tagController.deleteTag);
 
-router.get('/:id/notes', verifyJWT, tagController.getNotesByTag)
+router.get('/:id/notes', verifyJWT, tagController.getNotesByTag);
 
-router.get('/:id/autocomplete/:query', verifyJWT, tagController.autoCompleteTagNames)
+router.get(
+  '/:id/autocomplete/:query',
+  verifyJWT,
+  tagController.autoCompleteTagNames
+);
 
-module.exports = router
+module.exports = router;
